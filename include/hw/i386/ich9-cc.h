17ca6f314ad120f5a0eecbb72391d37c838b03ae
Paulo Alcantara <pcacjr@zytor.com>
2015-05-25 17:40:13 -0300

    target-i386: reserve RCRB mmio space in ACPI DSDT table
    This block is mapped into memory space, using the Root Complex Base
    Address (RCBA) register of the PCI-to-LPC bridge. Accesses in this space
    must be limited to 32-(DW) bit quantities. Burst accesses are not
    allowed.
    
    All Chipset Configuration Registers are located in this 16KiB space.
    
    Signed-off-by: Paulo Alcantara <pcacjr@zytor.com>
    ---
    v1 -> v2:
      * s/PDRC/CCR/ for clarity and match ICH9 spec
      * remove unnecessary OperationRegion for RCRB
    
    v2 -> v3: (no changes)
    
    v3 -> v4:
      * quote RCRB description from ICH9 spec to commit log
      * fix indentation issue in _CRS() method declaration
      * create hw/i386/ich9-cc.h for chipset configuration register values
        and use them in ASL
    
    v4 -> v5:
      * prefix macros in ich9-cc.h with "ICH9_" for better readability and
        make use of them in CCR device definition

---
+++ include/hw/i386/ich9-cc.h
@@ -0,0 +1,15 @@
+/*
+ * QEMU ICH9 Chipset Configuration Registers
+ *
+ * Copyright (c) 2015 Paulo Alcantara <pcacjr@zytor.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ */
+#ifndef HW_ICH9_CC_H
+#define HW_ICH9_CC_H
+
+#define ICH9_RCBA_BASE_ADDR    0xfed1c000
+#define ICH9_RCRB_SIZE         0x00004000
+
+#endif /* HW_ICH9_CC_H */
